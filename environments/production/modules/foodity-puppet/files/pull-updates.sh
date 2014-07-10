c3058537f1141677d56dd0115aab0db763f0b684
Claudio Benfatto <claudio.benfatto@foodity.com>
2014-07-10 15:03:50 +0000

    New pull-update

--- environments/production/modules/foodity-puppet/files/pull-updates.sh
+++ environments/production/modules/foodity-puppet/files/pull-updates.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 cd /etc/puppet
-<<<<<<< HEAD
 git checkout master && git clean -d -f && git checkout -- . && git pull && /usr/local/bin/papply
-=======
-git checkout master && git clean -d -f && git checkout -- . && git checkogit pull && /usr/local/bin/papply
->>>>>>> 5297efa661531ddaa31c53b594c9c2d7a1359d46
