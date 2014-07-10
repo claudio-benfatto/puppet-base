5297efa661531ddaa31c53b594c9c2d7a1359d46
Claudio Benfatto <claudio.benfatto@foodity.com>
2014-07-10 14:50:34 +0000

    Added git project cleanup when pulling from master. Added known_hosts addition for immediate connectivity to git server

--- environments/production/modules/foodity-puppet/files/pull-updates.sh
+++ environments/production/modules/foodity-puppet/files/pull-updates.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /etc/puppet
-git checkout master && git pull && /usr/local/bin/papply
+git checkout master && git clean -d -f && git checkout -- . && git checkogit pull && /usr/local/bin/papply
