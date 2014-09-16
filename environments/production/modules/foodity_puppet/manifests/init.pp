7c024994b44bff375d8d35a5d6b8105f81f32b1f
root <root@precise64.(none)>
2014-09-16 10:38:52 +0000

    Added vcsrepo to handle git revisions

--- environments/production/modules/foodity_puppet/manifests/init.pp
+++ environments/production/modules/foodity_puppet/manifests/init.pp
@@ -21,31 +21,38 @@ class foodity_puppet {
   create_resources('ssh::server::host_key', hiera(host_ssh_key))
 
 
+  vcsrepo { '/etc/puppet':
+    ensure  => 'present',
+    provider => 'git',
+    source   => 'git@git.foodity.com:claudio.benfatto/puppet-automation.git',
+    revision => 'develop',
+  }
+
   file { '/usr/local/bin/papply':
     source => 'puppet:///modules/foodity_puppet/papply.sh',
     mode   => '0755',
   }
 
-  file { '/usr/local/bin/pull-updates':
-    source => 'puppet:///modules/foodity_puppet/pull-updates.sh',
-    mode   => '0755',
-}
+#  file { '/usr/local/bin/pull-updates':
+#    source => 'puppet:///modules/foodity_puppet/pull-updates.sh',
+#    mode  => '0755',
+#}
 
-  exec {'set-git-remote':
-    command => "git remote set-url origin ${ssh_git_remote}",
-    path    => '/usr/bin/:/bin/',
-    cwd     => '/etc/puppet',
-    unless  => "git remote show origin | grep ${ssh_git_remote} 2> /dev/null",
-    require => 'Package[git]',
-  }
+#  exec {'set-git-remote':
+#    command => "git remote set-url origin ${ssh_git_remote}",
+#    path    => '/usr/bin/:/bin/',
+#    cwd     => '/etc/puppet',
+#    unless  => "git remote show origin | grep ${ssh_git_remote} 2> /dev/null",
+#    require => 'Package[git]',
+#  }
 
   cron { 'run-puppet':
     ensure  => 'absent',
     user    => 'root',
-    command => '/usr/local/bin/pull-updates',
+    command => '/usr/local/bin/papply',
     minute  => '*/10',
     hour    => '*',
-    require => ['Exec[set-git-remote]', 'Sshkey[known_git.foodity.com_1]', 'Sshkey[known_git.foodity.com_2]']
+    require => ['Vcsrepo[/etc/puppet]', 'Sshkey[known_git.foodity.com_1]', 'Sshkey[known_git.foodity.com_2]'],
   }
 
 }
